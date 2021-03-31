31444b3e5ed78878a882f61485c53e62e4664c0c
Kennedy Eruba <erubakenny@gmail.com>
2021-03-31 14:33:57 +0000

    added dynamic tables

--- src/app/components/trade-view/trade-view.component.ts
+++ src/app/components/trade-view/trade-view.component.ts
@@ -15,22 +15,22 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 
 
 export class TradeViewComponent implements OnInit {
-  product:string
-  quantity:number
-  price:number
-  side:string
-  responseMessage:string
-  is_loading:boolean=false
-  is_submiting:boolean=false
-  customerOrders:any[]
-  customerPendingOrders:any[]
-  customerCompleteOrders:any[]
+  product: string
+  quantity: number
+  price: number
+  side: string
+  responseMessage: string
+  is_loading: boolean = false
+  is_submiting: boolean = false
+  customerOrders: any[]
+  customerPendingOrders: any[]
+  customerCompleteOrders: any[]
 
-  marketData:any[]
+  marketData: any[]
 
 
 
-  constructor(private orderService:OrderService, private portfolioService:PortfolioService, private authService:AuthenticationService) { }
+  constructor(private orderService: OrderService, private portfolioService: PortfolioService, private authService: AuthenticationService) { }
 
   ngOnInit(): void {
     this.authService.isUserLoggedIn()
@@ -38,87 +38,89 @@ export class TradeViewComponent implements OnInit {
     this.getCustomerPendingOrders()
     this.getCustomerCompleteOrders()
     this.getMarketData()
-    
+
   }
 
 
-  getCustomerOrders(){
-    this.is_loading=true;
-    this.orderService.getCustomerOrders( ).subscribe(response=>{
-      if(response.status!=200){
-        this.is_loading=false
-        this.customerOrders=[]
+  getCustomerOrders() {
+    this.is_loading = true;
+    this.orderService.getCustomerOrders().subscribe(response => {
+      if (response.status != 200) {
+        this.is_loading = false
+        this.customerOrders = []
       }
-      this.is_loading=false
+      // console.log(response.data)
+      this.is_loading = false
       this.customerOrders = response.data
-      console.log('Order: '+ this.customerOrders)
+      // console.log('Order: '+ this.customerOrders)
     })
   }
-  getCustomerPendingOrders(){
-    this.is_loading=true;
-    this.orderService.getCustomerPendingOrders( ).subscribe(response=>{
-      if(response.status!=200){
-        this.is_loading=false
-        this.customerPendingOrders=[]
+  getCustomerPendingOrders() {
+    this.is_loading = true;
+    this.orderService.getCustomerPendingOrders().subscribe(response => {
+      if (response.status != 200) {
+        this.is_loading = false
+        this.customerPendingOrders = []
       }
-      this.is_loading=false
+      this.is_loading = false
       this.customerPendingOrders = response.data
-      console.log('Pending Order: '+ this.customerPendingOrders)
+      // console.log('Pending Order: ' + this.customerPendingOrders)
+      // this.customerPendingOrders.forEach(x => console.log(x))
     })
   }
 
-  getCustomerCompleteOrders(){
-    this.is_loading=true;
-    this.orderService.getSuccessfulCustomerOrders( ).subscribe(response=>{
-      if(response.status!=200){
-        this.is_loading=false
-        this.customerCompleteOrders=[]
+  getCustomerCompleteOrders() {
+    this.is_loading = true;
+    this.orderService.getSuccessfulCustomerOrders().subscribe(response => {
+      if (response.status != 200) {
+        this.is_loading = false
+        this.customerCompleteOrders = []
       }
-      this.is_loading=false
+      this.is_loading = false
       this.customerCompleteOrders = response.data
-      console.log('Complete Order: '+ this.customerCompleteOrders)
-      
+      // console.log('Complete Order: '+ this.customerCompleteOrders)
+
     })
   }
-  createOrder(){
-    const orderDetails:Order={
-      price:this.price,
-      quantity:this.quantity,
-      side:this.side,
-      product:this.product,
-      customerId:localStorage.getItem('userId')
-      
+  createOrder() {
+    const orderDetails: Order = {
+      price: this.price,
+      quantity: this.quantity,
+      side: this.side,
+      product: this.product,
+      customerId: localStorage.getItem('userId')
+
     }
-    this.is_submiting=true
-    this.orderService.createOrder(orderDetails).subscribe(response=>{
-      if(response.status!=200){
-        this.is_loading=false
-        this.responseMessage='error submitting order. Please try again'
+    this.is_submiting = true
+    this.orderService.createOrder(orderDetails).subscribe(response => {
+      if (response.status != 200) {
+        this.is_loading = false
+        this.responseMessage = 'error submitting order. Please try again'
         return
       }
-      this.is_submiting=false
-      this.responseMessage=response.message
-      console.log(response)
+      this.is_submiting = false
+      this.responseMessage = response.message
+      // console.log(response)
     })
 
   }
 
-  addItemToPortfolio(orderId:number){
-    const portfolioRequest:PortfolioRequest={
-      order_id:orderId,
-      customer_id:localStorage.getItem('userId')
+  addItemToPortfolio(orderId: number) {
+    const portfolioRequest: PortfolioRequest = {
+      order_id: orderId,
+      customer_id: localStorage.getItem('userId')
 
     }
-  this.portfolioService.addItemToPortfolio(portfolioRequest).subscribe(response=>{
-    console.log(response.data)
-  })
+    this.portfolioService.addItemToPortfolio(portfolioRequest).subscribe(response => {
+      console.log(response.data)
+    })
 
-}
+  }
 
-getMarketData(){
-  this.orderService.getMartketData().subscribe(response=>{
-    this.marketData=response
-  })
-}
+  getMarketData() {
+    this.orderService.getMartketData().subscribe(response => {
+      this.marketData = response
+    })
+  }
 
 }
